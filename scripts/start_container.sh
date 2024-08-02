0513b85066cfef4e0f3b4689a201ec20eea1a375
vaisharigela-git <vaisharigela@gmail.com>
2024-08-02 18:58:29 +0530

    final commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull vaisharig/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vaisharig/simple-python-flask-app
+docker run -d -p 5000:5000 vaisharig/simple-python-flask-app 
