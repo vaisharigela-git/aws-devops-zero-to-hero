68761e744c5c2bac65e67edfd76cf6305bfa2dfa
vaisharigela-git <vaisharigela@gmail.com>
2024-08-05 13:00:38 +0530

    final1

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vaisharig/simple-python-flask-app  
+docker pull vaisharig/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vaisharig/simple-python-flask-app
